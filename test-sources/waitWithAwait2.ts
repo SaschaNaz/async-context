51a44325790c831cb35a81803eedc32a2d7d5598
Kagami Sascha Rosylight <saschanaz@outlook.com>
2015-12-30 01:56:08 +0900

    await

--- test-sources/waitWithAwait2.ts
+++ test-sources/waitWithAwait2.ts
@@ -13,13 +13,11 @@ function waitFor(millisecond: number) {
     })
 }
 
-function awaitWaiter() {
+function awaitWaiter2() {
     return new CancellableContext<boolean>(async (context) => {
-        let result = await context.queue(() => waitFor(100)).queue((value) => {
-            return value === Cancellables.cancellation ? "cancel" : "uncancel";
-        }, { behaviorOnCancellation: "none" });
+        let result = await context.queue(() => waitFor(100));
 
-        if (result !== "cancel") {
+        if (result !== Cancellables.cancellation) {
             context.resolve(false);
         }
         else {
@@ -28,17 +26,17 @@ function awaitWaiter() {
     }, { deferCancellation: true }).feed();
 }
 
-describe("Waiting 100 ms with await", function() {
+describe("Waiting 100 ms with await 2", function() {
     it("should call done", function(done) {
         let now = Date.now();
-        let logger = awaitWaiter();
+        let logger = awaitWaiter2();
         logger.then(value => {
             chai.assert(!value, "value should be false");
             chai.assert((Date.now() - now) > 90, "at least 90 ms");
         }).then(done, done);
     });
     it("should be canceled", function(done) {
-        let logger = awaitWaiter();
+        let logger = awaitWaiter2();
         logger.then(value => {
             chai.assert(value, "value should be true");
         }).then(done, done);
--- test-sources/waitWithAwait3.ts
+++ test-sources/waitWithAwait3.ts
@@ -13,13 +13,13 @@ function waitFor(millisecond: number) {
     })
 }
 
-function awaitWaiter() {
+let awaitWaiter3 = function awaitWaiter3() {
     return new CancellableContext<boolean>(async (context) => {
-        let result = await context.queue(() => waitFor(100)).queue((value) => {
-            return value === Cancellables.cancellation ? "cancel" : "uncancel";
-        }, { behaviorOnCancellation: "none" });
+        await waitFor(100);
 
-        if (result !== "cancel") {
+        let result = await context.queue(() => Promise.resolve());
+
+        if (result !== Cancellables.cancellation) {
             context.resolve(false);
         }
         else {
@@ -28,17 +28,17 @@ function awaitWaiter() {
     }, { deferCancellation: true }).feed();
 }
 
-describe("Waiting 100 ms with await", function() {
+describe("Waiting 100 ms with await 3", function() {
     it("should call done", function(done) {
         let now = Date.now();
-        let logger = awaitWaiter();
+        let logger = awaitWaiter3();
         logger.then(value => {
             chai.assert(!value, "value should be false");
             chai.assert((Date.now() - now) > 90, "at least 90 ms");
         }).then(done, done);
     });
     it("should be canceled", function(done) {
-        let logger = awaitWaiter();
+        let logger = awaitWaiter3();
         logger.then(value => {
             chai.assert(value, "value should be true");
         }).then(done, done);
